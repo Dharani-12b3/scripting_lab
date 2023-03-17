731e432943ea4ca0d078eff52cc93b69ce3f733d
Dharani-12b3 <85936102+Dharani-12b3@users.noreply.github.com>
2023-03-17 10:05:02 +0530

    Create area_perimeter.rb

---
+++ area_perimeter.rb
@@ -0,0 +1,6 @@
+puts "enter the radius ";
+radius = gets.chomp.to_f;
+area = 3.14*radius*radius;
+puts "The area of the circle #{area}";
+perimeter = 2*3.14*radius;
+puts "The perimeter of the circle #{perimeter}"
